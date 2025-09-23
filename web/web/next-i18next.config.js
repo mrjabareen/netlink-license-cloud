faa2d410e4edd17be92063e3f2b2d58bf61445b1
mrjabareen <mrjabarin@gmail.com>
2025-09-23 04:22:44 +0300

    Delete web directory

--- web/web/next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'ar',
-    locales: ['ar', 'en'],
-    localeDetection: true,
-  },
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-}
